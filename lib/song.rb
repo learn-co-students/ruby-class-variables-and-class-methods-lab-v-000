9e33596a8bfc39641cf89f7f76ac5d82afa412ea
selichter <lichter.sarahe@gmail.com>
2015-11-18 18:45:11 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+  attr_reader :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each_with_object(Hash.new(0)) { |genre, hash| hash[genre] += 1 }
+  end
+
+  def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) { |artist, hash| hash[artist] += 1 }
+  end
+
+end
