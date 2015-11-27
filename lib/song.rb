78eac7cc87cd4cca3930df9b0b6abb1dbf91f481
Tracy <tracetrace@gmail.com>
2015-11-27 08:02:36 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+  attr_reader :name, :genre, :artist
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] ? genre_count[genre] += 1 : genre_count[genre] = 1
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+      artist_count[artist] ? artist_count[artist] += 1 : artist_count[artist] = 1
+    end
+    artist_count
+  end
+
+end
