6364b50b73e554e65e6da02883028135fc573e4b
babyshoes <dianecai@gmail.com>
2015-11-28 11:03:33 -0500

    all done

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.map do |genre|
+      genre_count[genre] ||= 0
+      genre_count[genre] += 1
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.map do |artist|
+      artist_count[artist] ||= 0
+      artist_count[artist] += 1
+    end
+    artist_count
+  end
+
+end
