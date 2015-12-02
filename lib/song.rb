b1cc229aa258b4e11eef44edb6aed136c461df47
lisamarie616 <lisamarie616@gmail.com>
2015-12-02 12:57:07 -0800

    revise #self.genre_count and #self.artist_count

--- lib/song.rb
+++ lib/song.rb
@@ -26,19 +26,35 @@ class Song
     @@artists.uniq
   end
 
+  # def self.genre_count
+  #   genre_count = {}
+  #   @@genres.each do |genre|
+  #     genre_count[genre] ||= genre_count[genre] = 0
+  #     genre_count[genre] += 1
+  #   end
+  #   genre_count
+  # end
+
+  # def self.artist_count
+  #   artist_count = {}
+  #   @@artists.each do |artist|
+  #     artist_count[artist] ||= artist_count[artist] = 0
+  #     artist_count[artist] += 1
+  #   end
+  #   artist_count
+  # end
+
   def self.genre_count
-    genre_count = {}
+    genre_count = Hash.new(0)
     @@genres.each do |genre|
-      genre_count[genre] ||= genre_count[genre] = 0
       genre_count[genre] += 1
     end
     genre_count
   end
 
   def self.artist_count
-    artist_count = {}
+    artist_count = Hash.new(0)
     @@artists.each do |artist|
-      artist_count[artist] ||= artist_count[artist] = 0
       artist_count[artist] += 1
     end
     artist_count
