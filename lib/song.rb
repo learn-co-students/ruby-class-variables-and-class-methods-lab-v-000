05d58d66cc9f1c7a577f7fbace1192c45f0f114c
febbraiod <donedesign1@gmail.com>
2015-12-18 12:53:03 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @@count += 1
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+
+    @@genres.each do |genre|
+    genre_count.has_key?(genre) ? genre_count[genre] += 1 : genre_count[genre] = 1
+    end
+  
+   genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+    artist_count.has_key?(artist) ? artist_count[artist] += 1 : artist_count[artist] = 1
+    end
+  
+    artist_count
+  end
+
+
+end
