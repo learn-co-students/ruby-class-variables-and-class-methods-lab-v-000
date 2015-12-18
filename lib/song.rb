fa3e997d1322025a681ee0f35d893f2bea28ad0c
James Novak <jamesnovak90@gmail.com>
2015-12-17 22:48:48 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+require 'pry'
+class Song
+
+attr_accessor :name, :artist, :genre
+
+@@count = 0
+@@genres = []
+@@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+      @@count += 1
+      @@genres << genre
+      @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.genre_count
+    histogram = Hash.new(0)
+    @@genres.each do |genre| 
+      histogram[genre] += 1
+    end
+    histogram
+  end
+
+  def self.artist_count
+    histogram = Hash.new(0)
+    @@artists.each do |artist| 
+      histogram[artist] += 1
+    end
+    histogram
+  end
+
+end
