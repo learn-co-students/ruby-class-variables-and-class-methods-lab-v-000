f0ced995767f01911fe9f835d91e5c353024bec4
ryuichi7 <brettrheenan@gmail.com>
2015-12-25 17:33:30 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,9 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count.include?(genre)
-        genre_count[genre] = genre_count[genre] ? + 1 : 1
+        genre_count[genre] = genre_count[genre] + 1
+      else
+        genre_count[genre] = 1
       end
     end
     genre_count
