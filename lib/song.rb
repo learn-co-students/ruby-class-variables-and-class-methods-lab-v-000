47b7969c1dfde2b15707648598b554df0e251319
jamcelr <jamcelr@hotmail.com>
2015-12-28 12:48:49 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,7 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@artist_count = {}
-  @@genre_count = {}
-
+  
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
@@ -29,6 +27,7 @@ class Song
   end
 
   def self.genre_count
+    @@genre_count = {}
     @@genres.each do |genre|
       if @@genre_count.include?(genre)
         @@genre_count[genre] += 1
@@ -40,6 +39,7 @@ class Song
   end
 
   def self.artist_count
+    @@artist_count = {}
     @@artists.each do |artist|
       if @@artist_count.include?(artist)
         @@artist_count[artist] += 1
