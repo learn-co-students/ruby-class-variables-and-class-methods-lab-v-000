138dfd3df5aa13f4928f3ee1f99f191102724fa3
pajamaw <pjpeterww@gmail.com>
2016-01-12 15:10:49 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+
+  @@artists = []
+
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+
+    @@artists << artist
+
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each do |genre| 
+     genre_hash[genre] += 1 
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = Hash.new(0)
+    @@artists.each do |artist| 
+     artist_hash[artist] += 1 
+    end
+    artist_hash
+  end
+
+end
