746fa535ac9a29a64452e3bb0f301f5de0c9a45d
Ben C <bcwong1@ualberta.ca>
2016-01-12 17:52:17 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres.push(genre)
+    @@artists.push(artist)
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each do |genre|
+      genre_hash[genre] += 1
+    end
+    genre_hash
+  end
+
+ def self.artist_count
+    artist_hash = Hash.new(0)
+   @@artists.each do |artist|
+     artist_hash[artist] += 1
+   end
+    artist_hash
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
