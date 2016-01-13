e266857e1a666d9b48b039bdd55af99e523185b1
jaeku <ja.ku@rutgers.edu>
2016-01-13 05:29:17 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+   @@genres.each do |genre|
+     @@genre_count[genre] = @@genres.count(genre)
+   end
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      @@artist_count[artist] = @@artists.count(artist)
+    end
+    @@artist_count
+  end
+
+end
