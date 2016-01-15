7fc7ea6a07153e9658188224105334301604226e
phamducv <phamducv@gmail.com>
2016-01-15 04:34:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
 class Song
-
         attr_accessor :name, :artist, :genre
 @@count=0
-        @@genres=[]
         @@artists=[]
+        @@genres=[]
         def initialize(name, artist, genre)
                 @name=name
                 @artist=artist
                 @genre=genre
-@@count+=1
-                @@artists << @artist
-                @@genres << @genre
-end
+                @@count+=1
+                @@artists << artist
+                @@genres << genre
+        end
+
 
         def self.count
-           @@count
+                @@count
         end
 
         def self.artists
@@ -32,25 +32,22 @@ end
                                 genre_count[genre]+=1
                         else
                                 genre_count[genre]=1
-                        end
         end
+
+                end
                 genre_count
-        end
+                end
+         def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
 
-
-
-        def self.artist_count
-artist_count={}
-                @@artists.each do |artist|
-                        if artist_count[artist]
-                                artist_count[artist]+=1
-                        else
-                                artist_count[artist]=1
-
-        end
-
-end
-                artist_count
-        end
 end
