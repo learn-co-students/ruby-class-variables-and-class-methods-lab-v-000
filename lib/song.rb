35bf859e69ae790d9ed4b85d04c318d955b83415
glee38 <glee3388@gmail.com>
2016-01-20 20:07:14 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,7 @@
+require 'pry'
+
 class Song
+
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = Array.new
@@ -26,19 +29,39 @@ class Song
   end
 
   def self.genre_count
-    genre_count = Hash.new(0)
+    #genre_count = to and empty hash
+    #example:
+    #[rap, rap, pop], each do |genre|
+      #genre_count[rap] = [rap, rap, pop].count(rap)
+    #return updated genre_count hash
+
+    genre_count = Hash.new
     @@genres.each do |genre|
-      genre_count[genre] += 1
+      #creating new key/value pair
+      genre_count[genre] = @@genres.count(genre)
     end
+    genre_count
   end
 
   def self.artist_count
-    @@artists.uniq.inject({}) do |artist, count|
-      artist[count] = @@artists.count(count); artist
+    #the {} is the starting point for .inject in this case
+    #the ";" separates the two lines of code into separate lines. the "hash" after the semicolon is simply returning the updated hash
+
+    #the method broken down would look as follows if the @@artists array contained the following values, [jay-z, jay-z, britney]:
+
+    #1st iteration:
+    #hash = {}
+    #[jay-z, jay-z, britney].inject({}) do |hash, jay-z|
+      #hash[:jay-z] = [jay-z, jay-z, britney].count(jay-z); {"jay-z" => 2}
+
+    #more on .inject: http://blog.jayfields.com/2008/03/ruby-inject.html
+
+    @@artists.inject({}) do |hash, artist|
+      #creating new key/value pair
+      hash[artist] = @@artists.count(artist); hash
     end
   end
 
-
-
+binding.pry
 
 end
