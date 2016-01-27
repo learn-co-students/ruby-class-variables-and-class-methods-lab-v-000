cbf3782b7a5be702d8458ef22f4f6f8e7289b12d
Rafael A. Mejia <rafael@dataminded.net>
2016-01-27 04:27:05 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@artists = []
+  @@genres = []
+  @@count = 0
+  @@genre_hash = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres.push genre
+    @@artists.push artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash[genre] == nil
+        genre_hash[genre] = 1
+      else
+        genre_hash[genre] += 1
+      end
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash[artist] == nil
+        artist_hash[artist] = 1
+      else
+        artist_hash[artist] += 1
+      end
+    end
+    artist_hash
+  end
+
+  
+  
+end
