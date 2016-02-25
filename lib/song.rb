78de9680afb331608b989c176e9d0bf624c50ad0
CodeByLine <yleventhal@ymail.com>
2016-02-25 05:47:16 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+
+
+  end
+
+
+  def name
+    @name
+  end
+
+  def artist
+    @artist
+  end
+  
+  def genre
+    @genre
+  end
+
+
+
+
+
+
+  end
