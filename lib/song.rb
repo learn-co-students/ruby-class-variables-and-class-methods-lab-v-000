7cbb26f0764eecaf8023682caa866571eb7d9181
wooke2e <wooke2e@gmail.com>
2016-02-25 02:27:22 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) {|h, item| h[item] += 1; h}
+  end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) {|h, item| h[item] += 1; h}
+  end
+
+
+
+
+
+end
