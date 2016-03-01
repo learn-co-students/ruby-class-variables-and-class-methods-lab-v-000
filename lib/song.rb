d7736850ecc2f59cca836d6baf9b52caebf6a4f8
aunnam <ajit.unnam@berkeley.edu>
2016-03-01 00:42:49 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,5 +29,15 @@ class Song
     @@genres.uniq
   end
 
-  
+  def self.genre_count
+    genre_count={]
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count[genre]=1
+      end
+    end
+    genre_count
+  end
 end
