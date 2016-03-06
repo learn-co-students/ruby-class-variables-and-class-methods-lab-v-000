82c974c60c8700f7e43c7d1f005e268392a176e4
LonghornRach <LonghornRach@users.noreply.github.com>
2016-03-06 09:10:43 -0600

    final

--- lib/song.rb
+++ lib/song.rb
@@ -6,24 +6,51 @@ class Song
 	@@artists = []
 
 	def initialize(name, artist, genre)
+		@name = name
+		@artist = artist
+		@genre = genre
 		@@count += 1
-		@@genre << genre
-		@@artists << artist
+		@@genres << @genre
+		@@artists << @artist
 	end
 
 	def self.genres
 		output = []
-		@@genres.each { |genre| output << genre if !output.contains?(genre) }
+		@@genres.each { |genre| output << genre if !output.include?(genre) }
 		output
 	end
 
 	def self.artists
 		output = []
-		@@artists.each { |artist| output << artist if !output.contains?(artist) }
+		@@artists.each { |artist| output << artist if !output.include?(artist) }
 		output
 	end
 
-	def self.genre_count
+	def self.count
+		@@count
+	end
 
+	def self.genre_count
+		out_hash = {}
+		@@genres.each do |genre|
+			if out_hash.include?(genre)
+				out_hash[genre] = (out_hash[genre] + 1)
+			else
+				out_hash[genre] = 1
+			end
+		end
+		out_hash
+	end
+
+	def self.artist_count
+		out_hash = {}
+		@@artists.each do |artist|
+			if out_hash.include?(artist)
+				out_hash[artist] = (out_hash[artist] + 1)
+			else
+				out_hash[artist] = 1
+			end
+		end
+		out_hash
 	end
 end
