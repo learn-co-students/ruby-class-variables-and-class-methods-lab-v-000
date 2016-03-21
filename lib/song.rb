ec37da222b777f82d1cad107aa8098ef84d1da1f
jchu4483 <jchu4483@gmail.com>
2016-03-21 16:24:17 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,12 +29,19 @@ class Song
   end
 
   def self.genre_count
-     genre_count = Hash.new(0)
-     @@genres.each do |genre|
-         genre_count[genre] += 1
+     genre_count = Hash.new(0) #set empty hash with default values of 0 in k/v pairs
+     @@genres.each do |genre| #iterate through @@genres array
+         genre_count[genre] += 1 #increment and set genre_count key of [genre] by 1
        end
      genre_count
    end
+
+   #The challenging part of this lab is checking for duplicate genres and artists.
+   #In the above, I don't need to set a counter = 0, I simply have to set increment genre_count hash
+   #In the end, this acts as a counter in itself. It accounts for duplicates on the spot -
+   #If [genre] already exists in the hash, then increment it by 1,
+   #If it does not, then it will set it equal to 1.
+   #Repeat.
 
   def self.artist_count
     artist_count = Hash.new(0)
