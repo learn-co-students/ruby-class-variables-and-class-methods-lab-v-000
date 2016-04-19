75e0f9e2e7953d410ac6276f5611ae84ed1a6831
ludaires <ludaires@gmail.com>
2016-04-19 01:43:05 -0400

    Creaate initialize method, class variable count, genres and artists.

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+
+  attr_accessor :new, :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@genres << genre
+    @@artists << artist
+    @@count +=1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @genre_count[genres] = self.count
+  end
+
+end
