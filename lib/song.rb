97cebffb8f91f7ae86edacda63c88b108d46136d
ludaires <ludaires@gmail.com>
2016-04-19 18:45:43 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
 
   attr_accessor :new, :name, :artist, :genre
@@ -28,8 +30,45 @@ class Song
     @@artists.uniq
   end
 
-  def self.genre_count
-    @genre_count[genres] = self.count
+  # def self.genre_count
+  #   # binding.pry
+  #   hash_genre_count = {}
+  #   compare_genre = " "
+  #   count = 0
+
+  #   @@genres.uniq.each do |genre|
+  #      if compare_genre == genre
+  #       hash_genre_count[genre] = count+=1
+  #     else
+  #       hash_genre_count[genre] = 1
+  #     end
+  #   end
+  #   hash_genre_count
+  # end
+
+    def self.artist_count
+    artist_count = {}
+    @@artists.each do |singer|
+      if artist_count[singer]
+        artist_count[singer] += 1
+      else
+        artist_count[singer] = 1
+      end
+    end
+    artist_count
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
 
 end
