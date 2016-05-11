46400c8d6941de5a497ababcf383871cc6f7df3d
Jonathan Stone <jon_p_stone@yahoo.com>
2016-05-10 22:02:44 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song#
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def genre_count(genre)
+  
+  end
+
+  def artist_count(artist)
+
+  end
+
+end
