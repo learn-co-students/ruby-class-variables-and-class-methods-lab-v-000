7a0b61c156eb1cae9134538b2301efa3e05f62f2
Jonathan Stone <jon_p_stone@yahoo.com>
2016-05-10 22:03:54 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -20,20 +20,36 @@ class Song#
     @@count
   end
 
-  def self.genres
-    @@genres.uniq
-  end
-
   def self.artists
     @@artists.uniq
   end
 
-  def genre_count(genre)
-  
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
-  def artist_count(artist)
+  def self.genres
+    @@genres.uniq
+  end
 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 
 end
