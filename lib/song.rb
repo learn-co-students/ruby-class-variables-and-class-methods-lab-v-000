5a9ca06bf1bed3ddae09689a4b42d228163549f4
steveafrost <ffrostt@gmail.com>
2016-05-15 12:52:44 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,30 +5,49 @@ class Song
   @@artists = []
   @@genres = []
   
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
   def self.count
     @@count
   end
   
   def self.artists
-    @@artists
+    @@artists.uniq
   end
   
   def self.genres
-    @@genres
+    @@genres.uniq
   end
   
   def self.genre_count
-    genre_count_hash = {}
-    ##incomplete, update this
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] +=1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
   
-  def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-  @@count += 1
-  @@artists << artist unless @@artists.include?(artist)
-  @@genres << genre unless @@genres.include?(genre)
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
   
 end
