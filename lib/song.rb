0d7bda6d31a715045a7622f67b7e479306369eed
Sandy Hogan <svern99@aol.com>
2016-06-02 18:17:18 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @genres = []
+  @genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << self.genre
+    @@artists << self.artist
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+  end
+
+end
