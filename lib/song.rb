f956178fd96fcc8e032928aec386f555459f7436
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-11 22:42:08 +0000

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
 
   attr_accessor :name, :artist, :genre
@@ -16,6 +17,7 @@ class Song
     @@genres << @genre
   end
 
+  #class methods
   #returns the total number of songs
   def self.count
     @@count
@@ -23,13 +25,27 @@ class Song
 
   #return an array of all genres with no duplicates
   def self.genres
-    @@genres.uniq  #returns an array of all the artists of existing genres
+    @@genres.uniq!  #returns an array of all the artists of existing genres excluding duplicates
   end
 
   def self.artists
-    @@artists.uniq #returns an array of all the artists of existing songs
+    @@artists.uniq! #returns an array of all the artists of existing songs excluding duplicates
   end
-  #class methods
 
+  def self.genre_count
+    @genres_hash = Hash.new(0) #to avoid having the hash default to 0 with hash={} make it equal to 0
+    @@genres.each do | genre_name |
+      @genres_hash[genre_name]+=1 #add a key and a value at teh same time.
+    end
+    @genres_hash
+  end
+
+  def self.artist_count
+    @artists_hash = Hash.new(0) #to avoid having the hash default to 0 with hash={} make it equal to 0
+    @@artists.each do | artist_name |
+      @artists_hash[artist_name]+=1 #add a key and a value at the same time.
+    end
+    @artists_hash
+  end
 
 end
