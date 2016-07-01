a5f40f764f9f57db80e2ef44e8822a67675490cb
peterberwind <peterberwind@gmail.com>
2016-07-01 13:15:14 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,16 +14,41 @@ class Song
     @@artists << artist
   end
 
-  def count
+  def self.count
     @@count
   end
 
-  def genres
+  def self.genres
     unique_genres = @@genres.uniq
   end
 
-  def artists
+  def self.artists
     unique_artists = @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      # the genre exists
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else # the genre doesnt exsist
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artists_count = {}
+    @@artists.each do |artist|
+      if artists_count[artist]
+        artists_count[artist] += 1
+      else
+        artists_count[artist] = 1
+      end
+    end
+    artists_count
   end
 
 
