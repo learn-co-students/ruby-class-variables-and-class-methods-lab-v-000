c02a648feceadb5fc386a81322111ab4b3078010
William Jones <enquiries@forestfed.com>
2016-08-01 17:32:30 +0000

    use class variables and methods

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash.has_key?(genre)
+        genre_hash[genre] += 1
+      else
+        genre_hash[genre] = 1
+      end
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash.has_key?(artist)
+        artist_hash[artist] += 1
+      else
+        artist_hash[artist] = 1
+      end
+    end
+    artist_hash
+  end
+end
