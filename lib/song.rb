da09165e0158175142e1e8aae329e2a5dccac4de
Elias Ventura <eliasventura23@gmail.com>
2016-08-10 00:57:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,22 @@
 class Song
+
   attr_accessor :name, :artist, :genre
 
+@@artists = []
 @@count = 0
 @@genres = []
-@@artists = []
 
-
-  def initialize(name, artist, genre)
+  def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
     @@genres << genre
     @@artists << artist
+  end
+
+  def self.genres
+    @@genres.uniq
   end
 
   def self.count
@@ -23,8 +27,21 @@ class Song
     @@artists.uniq
   end
 
-  def self.genres
-    @@genres.uniq
+  def self.genre_count
+   genre_count = {}
+   @@genres.each do |genre|
+     if genre_count[genre]
+       genre_count[genre] += 1
+     else
+       genre_count[genre] = 1
+     end
+   end
+   genre_count
+ end
+
+
+def artist
+  @artist
 end
 
 def self.artist_count
@@ -37,21 +54,5 @@ def self.artist_count
      end
    end
    artist_count
- end
-
- def self.genres
-   @@genres.uniq
- end
-
- def self.genre_count
-   genre_count = {}
-   @@genres.each do |genre|
-     if genre_count[genre]
-       genre_count[genre] += 1
-     else
-       genre_count[genre] = 1
-     end
-   end
-   genre_count
  end
 end
