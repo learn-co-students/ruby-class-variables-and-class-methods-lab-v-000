8360f92a9a779481ceb6894a65b3ab207d21e4aa
Benjamin Cantlupe <bjcantlupe@gmail.com>
2016-09-03 19:38:49 +0000

    Song class

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
+    @@genres.each do |genre|
+      if @@genre_count.has_key?(genre)
+        @@genre_count[genre] += 1
+      else
+        @@genre_count[genre] = 1
+      end
+    end
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      if @@artist_count.has_key?(artist)
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end
+    end
+    @@artist_count
+  end
+end
