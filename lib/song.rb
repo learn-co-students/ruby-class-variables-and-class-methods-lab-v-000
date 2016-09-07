4aceed08fb73f5b61c7d88fa6484c1081fb4fa07
vanita eckert <vanita112003@gmail.com>
2016-09-07 22:33:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,19 @@ class Song
   def self.artist_count
     @@artists.each_with_object(Hash.new(0)) do |artist, count|
       count[artist] += 1
+      # or another method would be :
+      # def self.artist_count
+      #     artist_count = {}
+      #     @@artists.each do |artist|
+      #       if artist_count[artist]
+      #         artist_count[artist] += 1
+      #       else
+      #         artist_count[artist] = 1
+      #       end
+      #     end
+      #     artist_count
+      #   end
+
     end
   end
 
