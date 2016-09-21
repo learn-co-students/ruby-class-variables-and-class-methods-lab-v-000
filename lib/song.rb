704617e4bbe7c69503ba7dad74fec4dd3451b5c2
Neil Ryan <neilmarkoliverryan@gmail.com>
2016-09-21 11:43:11 -0700

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -8,63 +8,62 @@ class Song
 
 	def initialize(name = nil, artist = nil, genre = nil)
 		@name = name
-		@artist = artist
+		@artist = artist	
 		@genre = genre
 
-    @@count += 1
-    @@genres << genre
-    @@artists << artist
-
-  end
+   		@@count += 1
+		@@genres << genre
+		@@artists << artist
+	end
 
   	def self.count
-    	@@count
+    		@@count
   	end
 
   	def self.genres
-			unique_genres = []
-			@@genres.each do |x|
-				if !unique_genres.include?(x)
-					unique_genres << x
-				end
+		unique_genres = []
+		@@genres.each do |x|
+			if !unique_genres.include?(x)
+				unique_genres << x
 			end
-			return unique_genres
+		end
+	return unique_genres
   	end
 
-		def self.artists
+	def self.artists
   		unique_artists = []
-			@@artists.each do |x|
-				if !unique_artists.include?(x)
-					unique_artists << x
-				end
+		@@artists.each do |x|
+			if !unique_artists.include?(x)
+				unique_artists << x
 			end
-			return unique_artists
+		end
+	return unique_artists
   	end
 
-		def self.genre_count
-			genre_hash = {}
-			count = 1
-			@@genres.each do |x|
-				if !genre_hash.has_key?(x)
-					genre_hash[x] = count
-				else
-					genre_hash[x] = count + 1
-				end
+	def self.genre_count
+		genre_hash = {}
+		count = 1
+		@@genres.each do |x|
+			if !genre_hash.has_key?(x)
+				genre_hash[x] = count
+			else
+				genre_hash[x] = count + 1		
 			end
-			return genre_hash
 		end
+	return genre_hash
+	end
 
-		def self.artist_count
-			artist_hash = {}
-			count = 1
-			@@artists.each do |x|
-				if !artist_hash.has_key?(x)
-					artist_hash[x] = count
-				else
-					artist_hash[x] = count + 1
-				end
+	def self.artist_count
+		artist_hash = {}
+		count = 1
+		@@artists.each do |x|
+			if !artist_hash.has_key?(x)
+				artist_hash[x] = count
+			else
+				artist_hash[x] = count + 1
 			end
-			return artist_hash
 		end
+	return artist_hash
+	end
 
 end
