eca05cfc495198907edab545b07859f47bf576e4
Bruno Boehm <bruno.boehm@gmail.com>
2016-10-21 05:57:34 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.uniq.each do |genre|
+      genre_count[genre] =  @@genres.select {|i| i == genre}.length
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count =  {}
+    @@artists.uniq.each do |artist|
+      artist_count[artist] = @@artists.select {|i| i == artist}.length
+    end
+    artist_count
+  end
+end
