d0d78fe0d15739ca2ce27093d00c6503eaf3f110
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-01-18 03:12:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,54 @@
 class Song
-  attr_accessor :name, :artist, :genre
+attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+@@count = 0
+@@artists = []
+@@genres = []
 
-def initialize
-@@song_count += 1
-end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count +=1
+    @@artists << artist
+    @@genres << genre
+  end
 
 def self.count
-  @@song_count
+  @@count
 end
 
+def self.artists
+  @@artists.uniq!
+end
 
+def self.genres
+  @@genres.uniq!
+end
 
+def self.genre_count
+  genre_count = {}
+     @@genres.each do |genre|
+       if genre_count[genre]
+         genre_count[genre] += 1
+       else
+         genre_count[genre] = 1
+       end
+     end
+     genre_count
+end
+
+def self.artist_count
+  artist_count = {}
+     @@artists.each do |artist|
+       if artist_count[artist]
+         artist_count[artist] += 1
+       else
+         artist_count[artist] = 1
+       end
+     end
+     artist_count
+end
 
 end
