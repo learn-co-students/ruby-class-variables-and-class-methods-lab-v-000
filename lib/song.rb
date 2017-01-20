1e86c1a20bd32abe36ad8926d715a1ff2cef7138
Kristi Thompson <kristiforpresident@gmail.com>
2017-01-20 21:38:57 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,68 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@genres = []
+  @@artists = []
+  @@count = 0
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << @artist
+    @genre = genre
+    @@genres << @genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    artists = []
+    @@artists.each do |artist|
+      if !artists.include?(artist)
+        artists << artist
+      end
+    end
+    artists
+  end
+
+  def self.genres
+    genres = []
+    @@genres.each do |genre|
+      if !genres.include?(genre)
+        genres << genre
+      end
+    end
+    genres
+  end
+
+  def self.genre_count
+    genre_count = {}
+
+    @@genres.each do |genre|
+      if genre_count.include?(genre)
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count  
+  end
+
+  def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+      if artist_count.include?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+end
