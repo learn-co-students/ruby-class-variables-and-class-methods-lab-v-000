c4b1869c5c84cfa3ebb3ed146a17474c1c27bc6c
Alex Crisan <alexcrisan2005@hotmail.com>
2017-02-14 17:45:19 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def count
+    return @@count
+  end
+
+  def genres
+    @@genres
+  end
+
+  def artists
+
+  end
+
+  def genre_count
+
+  end
+
+
+#end of class
+end
