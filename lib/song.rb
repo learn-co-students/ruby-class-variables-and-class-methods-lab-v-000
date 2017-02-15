1f9b2dbf4905ceadeae731d476304b52c7887579
Alex Crisan <alexcrisan2005@hotmail.com>
2017-02-15 17:51:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist, :genre
+  attr_writer :count
 
   @@count = 0
   @@genres = []
@@ -14,22 +15,41 @@ class Song
     @@artists << artist
   end
 
-  def count
+  def self.count
     @@count
   end
 
-  def genres
-    @@genres
+  def self.genres
+    @@genres.uniq
   end
 
-  def artists
-
+  def self.artists
+    @@artists.uniq
   end
 
-  def genre_count
-
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count.key?(genre) == false
+        genre_count[genre] = 1
+      else
+        genre_count[genre] += 1
+      end
+    end
+    return genre_count
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count.key?(artist) == false
+        artist_count[artist] = 1
+      else
+        artist_count[artist] += 1
+      end
+    end
+    return artist_count
+  end
 
 #end of class
 end
