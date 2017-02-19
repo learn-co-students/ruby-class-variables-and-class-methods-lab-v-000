4c9eb33e026e6062604076e38add89b6a7c041c6
Jessica Widman <jawidman.30@gmail.com>
2017-02-19 11:01:41 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    Hash[*@@genres.group_by{|genre| genre }.flat_map{ |genre_key, genre_count| [genre_key, genre_count.size]}]
+  end
+
+  def self.artist_count
+    Hash[*@@artists.group_by{|artist| artist }.flat_map{ |artist_key, artist_count| [artist_key, artist_count.size]}]
+  end
+end
