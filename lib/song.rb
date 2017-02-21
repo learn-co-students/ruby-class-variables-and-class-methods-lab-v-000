578f1915f7686cbf4f0a545cd591c3db345ee9cc
Mason Ellwood <masonellwood@gmail.com>
2017-02-21 23:35:29 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+
+    @@genres.each do |i|
+      if !genre_count.include?(i)
+        genre_count[i] = 1
+      else
+        genre_count[i] += 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |i|
+      if !artist_count.include?(i)
+        artist_count[i] = 1
+      else
+        artist_count[i] += 1
+      end
+    end
+    artist_count
+  end
+end
