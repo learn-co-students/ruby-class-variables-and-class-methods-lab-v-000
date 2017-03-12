e89d11ff588bb4ffb4f40ded8f27053818e031c8
Spencer Evans <spencevans@gmail.com>
2017-03-12 01:42:38 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genre_count
+    @@genres.each_with_object(Hash.new(0)) do |key, hash|
+      hash[key] += 1
+    end
+  end
+
+  def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) do |key, hash|
+      hash[key] += 1
+    end
+  end
+
+  def self.artists
+    @@artists.uniq.map
+  end
+
+  def self.genres
+    @@genres.uniq.map
+  end
+
+end
