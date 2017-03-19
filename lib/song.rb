417b83aa397a6a0892fd1fb7434f77f346e4537a
thesilentc <thesilentc@me.com>
2017-03-18 22:18:36 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+   attr_accessor :name, :artist, :genre
+
+   @@count = 0
+   @@genres = []
+   @@artists = []
+
+   def initialize(song_name, artist, genre)
+     @name = song_name
+     @artist = artist
+     @genre = genre
+     @@count += 1
+     @@genres << genre
+     @@artists << artist
+
+#
+#   def self.count
+#     @@count
+#   end
+#
+# end
+#
+# def song_name=(name)
+#     @song_name = name
+# end
+#
+#   def song_name
+#     @song_name
+#   end
+#
+#   def artist=(artist)
+#       @artist = artist
+#   end
+#
+#     def artist
+#       @artist
+#     end
+
+# class Album
+#   @@album_count = 0
+#
+#   def initialize
+#     @@album_count += 1
+#   end
+#
+#   def self.count
+#     @@album_count
+#   end
+  end
+end
