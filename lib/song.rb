7f3cfa4bc0e0ab7a23c2ca320af742f0b886804a
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-25 15:38:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+
+      @@artists << artist
+      @@genres << genre
+
+      @@count += 1
+    end
+
+    def self.count
+      @@count
+    end
+
+    def self.artists
+      @@artists.uniq
+    end
+
+    def self.genres
+      @@genres.uniq
+    end
+
+    def self.genre_count
+      genre_hash = Hash.new(0)
+      @@genres.each {|genre| genre_hash[genre] += 1}
+      genre_hash
+    end
+
+    def self.artist_count
+      artist_hash = Hash.new(0)
+      @@artists.each {|artist| artist_hash[artist] += 1}
+      artist_hash
+    end
+
+end
