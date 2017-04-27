dad444a6104d3bb0631e0082d13c9e2334da30e9
Will-Sauer <will@heueso.com>
2017-04-27 11:19:08 -0700

    on my very own environment

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,4 @@
-require 'pry'
-# require 'pry-nav'
+
 
 class Song
 #------------------------------------------------------------------------------------------------
@@ -8,29 +7,24 @@ class Song
 attr_accessor :name, :artist, :genre
 
 #class variables initialized
-@@count = 0
-@@artists = []
-@@genres = []
-@@library = {}
-
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    
 
 def initialize (name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-  @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
 
-
-  #to make it unique just add: if @@artists.empty?  || !@@artists.include?(artist)
-
+    #to make it unique just add: if @@artists.empty?  || !@@artists.include?(artist)
     @@artists << artist
 
-
-  #to make it unique just add: if @@genres.empty? || !@@genres.include?(genre)
+    #all genres
     @@genres << genre
 
-  #builds library hash of each song/artist/genre combo
-
+    
 
 end
 
@@ -39,43 +33,63 @@ end
 #------------------------------------------------------------------------------------------------
 # class methods
 #------------------------------------------------------------------------------------------------
-
 def self.count
 return @@count
 end
 
+#------------------------------------------------------------------------------------------------
 def self.artists
-  return @@artists.uniq
+return @@artists.uniq
 end
 
+#------------------------------------------------------------------------------------------------
 def self.genres
-  return @@genres.uniq
+return @@genres.uniq
 end
 
-
+#------------------------------------------------------------------------------------------------
 def self.genre_count
-@@genre_hash = {}
-@@genres.uniq.each {|current_genre|
-#for each unique genre I find
-#create a key in the hash
-binding.pry
-@@genre_hash[current_genre]= 0
-
-                        #for each unique genre, go through obj and count
-                        #all songs
-                        ObjectSpace.each_object(Song) {|song|
-                                                        binding.pry
-                                                        if song.genre == current_genre
-                                                        @@genre_hash[current_genre] += 1
-                                                        end
-                                                      }
+    @@genre_count_hash = {}
+    @@genres.each  {|current_genre|
+                    if @@genre_count_hash.has_key? (current_genre)
+                      @@genre_count_hash[current_genre] += 1
+                    else
+                      @@genre_count_hash[current_genre] = 1
+                    end
+                  }
 
 
-                }
-return @@genre_hash
-
-
+return @@genre_count_hash
 end
 
+
+#------------------------------------------------------------------------------------------------
+def self.genre_count
+    @@genre_count_hash = {}
+    @@genres.each  {|current_genre|
+                    if @@genre_count_hash.has_key? (current_genre)
+                      @@genre_count_hash[current_genre] += 1
+                    else
+                      @@genre_count_hash[current_genre] = 1
+                    end
+                  }
+
+
+return @@genre_count_hash
+end
+#------------------------------------------------------------------------------------------------
+def self.artist_count
+    @@artist_count_hash = {}
+    @@artists.each  {|current_artist|
+                    if @@artist_count_hash.has_key? (current_artist)
+                      @@artist_count_hash[current_artist] += 1
+                    else
+                      @@artist_count_hash[current_artist] = 1
+                    end
+                  }
+
+
+return @@artist_count_hash
+end
 
 end
