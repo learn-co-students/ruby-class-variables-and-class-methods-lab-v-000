35853b81fe72d518778461585b9fba2877bd8084
Graham Squires <graham.squires3@gmail.com>
2017-05-15 00:01:25 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,27 +30,4 @@ class Song
       end
 
         def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
-  end
+    
