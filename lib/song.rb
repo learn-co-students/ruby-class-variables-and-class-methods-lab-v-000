8c13d079c296ffe60ea22079ea166110a1f970e5
github username <github email address>
2017-05-23 16:52:29 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  @@genres = []
+  @@count = 0
+  @@artists = []
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.map do |genre|
+      if !genre_count[genre]
+        genre_count[genre] = 1
+      else
+        genre_count[genre] += 1
+      end
+    end
+    genre_count
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.map do |artist|
+      if !artist_count[artist]
+        artist_count[artist] = 1
+      else
+        artist_count[artist] += 1
+      end
+    end
+    artist_count
+  end
+end
