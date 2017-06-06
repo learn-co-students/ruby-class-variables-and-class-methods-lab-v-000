309ada1344bf2a7222be624437b1d1de4ef66433
Mel Chang <melanie.a.chang@gmail.com>
2017-06-06 08:01:23 +0000

    Done.

--- lib/song_test.rb
+++ lib/song_test.rb
@@ -4,7 +4,7 @@ def genre_count(array)
   genre_count = {}
   array.each do |genre|                #for each genre entered @ init
     if genre_count.has_key?(genre)     #does hash already include key?
-      genre_count[genre] += 1          #ifyes, add one to existing value
+      genre_count[genre] += 1          #if yes, add one to existing value
     else
       genre_count[genre] = 1           #otherwise, set key as 1
       binding.pry
