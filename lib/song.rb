69030e8f579e480ad31290201c4842bdc6af950e
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-08 17:01:18 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, e| total[e] +=1; total}
+    end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) {|total, e| total[e] += 1; total}
+  end
+
+
+end
