b51aa305d745f620214c74305618ba57d0dfa390
noson cohen <natancohen23@gmail.com>
2017-06-21 09:27:31 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,47 @@
 class Song
-  attr_accessor :name, :artist, :genre
 
-  @@count = 0
-  @@artists = []
-  @@genres = []
+attr_accessor :name, :artist, :genre
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
+@@count = 0
+@@genres = []
+@@artists = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+  @@genres << genre
+  @@artists << artist
+end
+
+def self.count
+  @@count
+end
+
+def self.genres
+  @@genres = @@genres.uniq
+end
+
+def self.artists
+  @@artists = @@artists.uniq
+end
+
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |gen|
+    genre_count[gen] = 0 if genre_count[gen].nil?
+    genre_count[gen] = genre_count[gen] + 1
   end
+    genre_count
+end
 
-  def self.count
-    @@count
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |count|
+      artist_count[count] = 0 if artist_count[count].nil?
+      artist_count[count] = artist_count[count] + 1
+    end
+      artist_count
   end
-
-  def self.genres
-    @@genres = @@genres.uniq
-  end
-
-  def self.artists
-    @@artists = @@artists.uniq
-  end
-
-  def self.genre_count
-    @@genres.each_with_object(Hash.new(0)) { |e, a| a[e] += 1 }
-  end
-
-  def self.artist_count
-    @@artists.each_with_object(Hash.new(0)) { |e, a| a[e] += 1 }
-  end
-
 end
