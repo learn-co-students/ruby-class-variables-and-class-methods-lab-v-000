d4b4b62ac2e2be03e6ae9507cd8ef75847d1444a
Justina Tran <justinavtran@yahoo.com>
2017-06-28 18:27:18 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |a|
+      if artist_count[a]
+        artist_count[a] += 1
+      else
+        artist_count[a] = 1
+      end
+    end
+      artist_count
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |g|
+      if genre_count[g]
+        genre_count[g] += 1
+      else
+        genre_count[g] = 1
+      end
+    end
+    genre_count
+  end
+end
