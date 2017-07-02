d3eb33cec88a1dc34071189bbeec67636583d1ae
Scyntre <github email address>
2017-07-02 01:00:08 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -25,11 +25,8 @@ class Song
 	def self.artist_count
 		artist_count = {}
 		@@artists.each do |artist|
-			if artist_count[artist]
-				artist_count[artist] += 1 
-			else
-				artist_count[artist] = 1
-			end
+			artist_count[artist] ||= 0
+			artist_count[artist] += 1
 		end
 		artist_count
 	end
@@ -41,11 +38,8 @@ class Song
 	def self.genre_count
 		genre_count = {}
 		@@genres.each do |genre|
-			if genre_count[genre]
-				genre_count[genre] += 1 
-			else
-				genre_count[genre] = 1
-			end
+			genre_count[genre] ||= 0
+			genre_count[genre] += 1
 		end
 		genre_count
 	end
