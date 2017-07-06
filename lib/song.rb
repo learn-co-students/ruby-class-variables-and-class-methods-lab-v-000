34d25502b99b93c9c89f3f0c7ddd7c2c2b1018a2
Josh  Burtnett <burtnett.josh@gmail.com>
2017-07-06 04:02:35 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      if hash[genre]
+        hash[genre] += 1
+      else
+        hash[genre] = 1
+      end
+    end
+    hash
+  end
+
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if hash[artist]
+        hash[artist] += 1
+      else
+        hash[artist] = 1
+      end
+    end
+    hash
+  end
+
+end
