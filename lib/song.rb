aae552a7e3cfbde49f69b103abdf5ea32b97928d
Eric Strickler <eric.strickler@gmail.com>
2017-07-07 22:41:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres #one way to do it
+    @@genres & @@genres
+  end
+
+  def self.artists #another way to do it
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] +=1
+      else genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] +=1
+      else artist_count[artist] =1
+      end
+    end
+    artist_count
+  end
+
+end
