3375ddf3dc455d047177e952752a6aa0f8699cc5
Annette Maza <nettymaza@gmail.com>
2017-08-24 20:56:56 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,61 @@
+
+require 'pry'
+
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    #uniq returns a new array by removing duplicate values in self.
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genres_hash = {}
+    @@genres.each do |genre|
+      if genres_hash[genre]
+        genres_hash[genre] += 1
+      else
+        genres_hash[genre] = 1
+      end
+    end
+    genres_hash
+  end
+
+def self.artist_count
+  artists_hash = {}
+  @@artists.each do |artist|
+    if artists_hash[artist]
+      artists_hash[artist] += 1
+    else
+      artists_hash[artist] = 1
+    end
+  end
+  artists_hash
+end
+
+
+
+end
