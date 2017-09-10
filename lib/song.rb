c59338b71e27b2bd2a83e5189371fb8577638660
Ruth Mesfun <ruth@flatironschool.com>
2017-09-10 17:30:36 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,43 @@
 class Song
   attr_reader :name, :artist, :genre
 
-  @@artists = Array.new
-  @@genres = Array.new
-  @@genre_count = Hash.new
-  @@artist_count = Hash.new
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
 
-  @@count = 1
+  @@count = 0
 
   def initialize(name, artist, genre)
+
     @name = name
     @artist = artist
     @genre = genre
 
     @@count +=1
+    @@artists << artist
+    @@genres << genre
 
-    @@artists << @artist
-    @@genres << @genre
+    unless @@genre_count.has_key?(genre)
+      @@genre_count[genre] = 1
+    else
+      @@genre_count[genre] += 1
+    end
+
+    unless @@artist_count.has_key?(artist)
+      @@artist_countt[artist] = 1
+    else
+      @@artist_countt[artist] += 1
+    end
+
   end
 
   def self.artists
-    @@artists
+    @@artists.uniq!
   end
 
   def self.genres
-    @@genres
+    @@genres.uniq!
   end
 
   def self.count
