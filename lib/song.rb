a9b631fcfebeae2bfec858026ef20ded63f0ea0e
Ariana Berger <ariana.berger@gmail.com>
2017-09-12 15:10:25 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |k|
+      if !genre_hash.key?(k)
+        genre_hash[k] = 1
+      else genre_hash[k] += 1
+        end
+      end
+      genre_hash
+    end
+
+    def self.artist_count
+      artist_hash = {}
+      @@artists.each do |k|
+      if !artist_hash.key?(k)
+        artist_hash[k] = 1
+      else artist_hash[k] += 1
+        end
+      end
+      artist_hash
+    end
+
+end
