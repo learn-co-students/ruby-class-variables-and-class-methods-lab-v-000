bcdf74766a29e51771d255686ebdeb8eb8aab056
Sergio Correa <correa.sergio@gmail.com>
2017-09-13 01:30:45 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(song, artist, genre)
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+    @name = song
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    artists = []
+    @@artists.map { |artist| artists << artist unless artists.include?(artist) }
+    artists
+  end
+#### ALTERNATIVE ####
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+end
