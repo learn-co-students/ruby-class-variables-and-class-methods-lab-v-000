1645333c3adba6c165184db92f4bdfd6278ee9b1
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-03 12:34:36 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+attr_reader :name, :artist, :genre
+@@count = 0
+@@artists = []
+@@genres = []
+
+
+def initialize(name, artist, genre)
+  @name=name
+  @artist=artist
+  @genre=genre
+  @@count +=1
+  @@artists << artist
+  @@genres << genre
+
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.count
+  @@count
+end
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.genre_count
+  genre_hash={}
+  @@genres.uniq.each do |unique_genre|
+    genre_hash[unique_genre]=@@genres.count(unique_genre)
+  end
+  genre_hash
+
+end
+
+def self.artist_count
+  artist_hash={}
+  @@artists.uniq.each do |unique_artist|
+    artist_hash[unique_artist]=@@artists.count(unique_artist)
+  end
+  artist_hash
+
+end
+
+
+end
