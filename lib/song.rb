a4d0d5dc18a2bc738e6ca1dd2cd07f9a261027b6
Christopher DiStasio <cadistasio@gmail.com>
2017-10-24 21:17:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,8 @@ class Song
     @@count = 0
     new_song = @@count +=1
     @@genres = [] ITERATE OVER THIS ARRAY AND POPULATE A HASH WITH THE KEY VALUE PAIRS FROM THE genre_count METHOD
+                  NEED TO CHECK THAT HASH ALREADY CONTAINS A KEY OF A PARTICULAR GENRE
+                  IF DOES CONTAIN THAT KEY, INCREMENT VALUE OF KEY BY ONE. IF NOT, CREATE A NEW KEY/VALUE PAIR
     @@genres << @genres
     @@artists = []
     @@artists << @artist
@@ -29,4 +31,9 @@ class Song
   def self.genre_count
     RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH GENRE, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT GENRE
   end
-  
+
+  def self.artist_count
+      RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH ARTIST, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT ARTIST
+  end
+
+end
