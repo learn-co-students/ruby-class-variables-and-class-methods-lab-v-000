540446580ffb577946fb43000a1fc0e4db0c0870
Christopher DiStasio <cadistasio@gmail.com>
2017-10-25 19:59:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,41 +1,45 @@
 class Song
   attr_accessor :name, :artist, :genre
-    @@count = 0
-  
+
+  @@count = 0
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    
-    new_song = @@count +=1
+    @@count +=1
     @@genres = []
+    @@genres << @genre
     # ITERATE OVER THIS ARRAY AND POPULATE A HASH WITH THE KEY VALUE PAIRS FROM THE genre_count METHOD
     #               NEED TO CHECK THAT HASH ALREADY CONTAINS A KEY OF A PARTICULAR GENRE
     #               IF DOES CONTAIN THAT KEY, INCREMENT VALUE OF KEY BY ONE. IF NOT, CREATE A NEW KEY/VALUE PAIR
-    @@genres << @genres
     @@artists = []
-    @@artists << @artist
     @@genre_count = {}
   end
 
   def self.count
-    # TOTAL NUMBER OF SONGS CREATED
+    @name.count# TOTAL NUMBER OF SONGS CREATED
   end
 
-  def self.genres
-    # NO RETURNING DUPLICATES
+  def self.genres(genre)
+    @genre[genre] ||= []
+    @@genres << @genre# NO RETURNING DUPLICATES
   end
 
   def self.artists
+    @artist[artist] ||= []
+    @@artists << @artist
     # NO CONTAINING DUPLICATES
   end
 
   def self.genre_count
+    @@genre_count << @@genres.each {|key, value| value.count}
     # RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH GENRE, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT GENRE
   end
 
   def self.artist_count
-      # RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH ARTIST, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT ARTIST
+    @@artist_count << @@artists.each {|key, value| value.count}
+    # RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH ARTIST, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT ARTIST
   end
 
 end
