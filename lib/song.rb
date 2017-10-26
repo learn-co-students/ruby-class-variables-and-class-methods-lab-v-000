b91ffe6e49f6d6020f40c093545dfcceae37c3ed
Christopher DiStasio <github email address>
2017-10-26 15:27:45 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,15 +11,9 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-
-    @@genres << @genre
     # ITERATE OVER THIS ARRAY AND POPULATE A HASH WITH THE KEY VALUE PAIRS FROM THE genre_count METHOD
     #               NEED TO CHECK THAT HASH ALREADY CONTAINS A KEY OF A PARTICULAR GENRE
     #               IF DOES CONTAIN THAT KEY, INCREMENT VALUE OF KEY BY ONE. IF NOT, CREATE A NEW KEY/VALUE PAIR
-
-    @@artists << @artist
-    @@count +=1
-
   end
 
   def self.count
@@ -28,22 +22,23 @@ class Song
 
   def self.genres(genre)
     @genre[genre] ||= []
-    @@genres << @genre# NO RETURNING DUPLICATES
+      # NO RETURNING DUPLICATESkey
   end
 
-  def self.artists
+  def self.artistskey
     @artist[artist] ||= []
-    @@artists << @artist
-    # NO CONTAINING DUPLICATES
+      # NO CONTAINING DUPLICATES
   end
 
   def self.genre_count
-    @@genre_count << @@genres.each {|key, value| value.count}
+    @@genre_count << @genre.each {|name, value|}
+    @@count +=1
     # RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH GENRE, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT GENRE
   end
 
   def self.artist_count
-    @@artist_count << @@artists.each {|key, value| value.count}
+    @@artist_count << @artist.each {|name, value|}
+    @@count +=1
     # RETURNS A HASH IN WHICH KEYS ARE NAMES OF EACH ARTIST, WHICH POINT TO A VALUE THAT IS THE NUMBER OF SONGS THAT HAVE THAT ARTIST
   end
 
