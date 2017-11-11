69ee67ecf48d9e4ec2151e9c9712377588173240
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-11 05:25:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   @@count = 0
   @@genres = []
   @@artists = []
-  attr_reader :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
