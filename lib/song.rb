e60c78decb5447ada24694f4a16a3dfa078d9ed8
charityyoungblood <youngblood.charity@gmail.com>
2017-11-15 09:37:35 -0800

    changed attr_accessor to attr_reader - added instance variables to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,12 @@ require 'pry'
 ## create class Song
 class Song
 # add attribute accessors for title, artist name and genre 
-  attr_accessor :title, :artist_name, :genre
+   attr_reader :title, :artist_name, :genre
 ## create initialize method, 
     def initialize(title, artist_name, genre)
-    
+      @title = title
+      @artist_name = artist_name
+      @genre = genre 
       
     end 
 ## upon initialization include
