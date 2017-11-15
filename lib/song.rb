d7c9b1de0bb44aacdec663ab79c4dcd5a9441e8d
charityyoungblood <youngblood.charity@gmail.com>
2017-11-15 09:23:46 -0800

    added add'l comments for project structure

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,20 @@
-# create class Song 
-
-# create initialize method, 
-# upon initialization include
-#  - song title 
-#  - artist name 
+## create class Song 
+#
+## create initialize method, 
+## upon initialization include
+##  - song title 
+##  - artist name 
+##  - genre 
+#
+#create instance methods for:
+#  - name 
+#  - artist 
 #  - genre 
+#  - count 
+#
+#create class methods for:
+#  - count 
+#  - artists
+#  - genres 
+#  - genre count
+#  - artist count
