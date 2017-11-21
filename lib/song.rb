43fdea94a43d14be2a050e4e7a243e3795cef118
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-21 11:38:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,27 @@ class Song
   end
 
   def self.genre_count
+    genre_histogram = {}
+    @@genres.each do |genre|
+      if genre_histogram[genre] == nil
+        genre_histogram[genre] = 1
+      else
+        genre_histogram[genre] += 1
+      end
+    end
+    genre_histogram
   end
 
   def self.artist_count
+    artist_histogram = {}
+    @@artists.each do |artist|
+      if artist_histogram[artist] == nil
+        artist_histogram[artist] = 1
+      else
+        artist_histogram[artist] += 1
+      end
+    end
+    artist_histogram
   end
 
 end
