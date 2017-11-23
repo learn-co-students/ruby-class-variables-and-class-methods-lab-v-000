6bc86f3b57cd3a08fe39ac039771d9ec1891090d
Remmy Clay <remmy.lauren@gmail.com>
2017-11-23 03:27:03 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count.has_key?(genre) ?
+      genre_count[genre]+=1 : genre_count[genre]= 1
+    end
+    genre_count
+  end
+
+  def self.artist_count
+   artist_count = {}
+    @@artists.each do |artist|
+      artist_count.has_key?(artist) ?
+      artist_count[artist]+=1 : artist_count[artist] = 1
+    end
+    artist_count
+  end
+
+end
