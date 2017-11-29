ad6c8b2a48a7ced82627b24f1b7a21e34ccdf2ee
Liz Burton <liz.burton147@gmail.com>
2017-11-29 12:58:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,31 @@ class Song
   end
 
   def self.genre_count
-
+    @@genre_count = {}
     @@genres.each do | genre |
-
+      if !@@genre_count.include?(genre)
+        @@genre_count[genre] = 1
+      else
+        @@genre_count[genre] += 1
+      end
     end
+    @@genre_count
   end
+
+  def self.artist_count
+    @@artist_count = {}
+    @@artists.each do | artist |
+      if !@@artist_count.include?(artist)
+        @@artist_count[artist] = 1
+      else
+        @@artist_count[artist] += 1
+      end
+    end
+    @@artist_count
+  end
+
 end
 
 hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
+lucifer = Song.new("Lucifer", "Jay-Z", "rap" ) 
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
