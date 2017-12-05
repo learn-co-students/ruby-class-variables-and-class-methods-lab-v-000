9c4b9b0b04492b5b38b30ec694b96fc07479929f
Trisha Garrett <trisha.garrett@gmail.com>
2017-12-05 20:57:03 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists
+  @@genres
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+
+  
+
+
+end
