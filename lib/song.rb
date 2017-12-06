3cc23fdd707636892fc7099ce1608be02f903edc
Phoebe Lau <phoebelau7@gmail.com>
2017-12-06 02:34:29 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_reader :name, :artist, :genre
+
+  def initialize(name,artist,genre)
+    @@count +=1
+    @name = name
+    @artist = artist
+    if @@artists.include?("artist") == false
+      @@artists << artist
+    end
+    @genre = genre
+    if @@genres.include?("genre") == false
+      @@genres << genre
+    end
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each {|genre|
+      genre_count[genre]||= genre_count[genre] =0
+      genre_count[genre] += 1
+    }
+  genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each {|artist|
+      artist_count[artist]||= artist_count[artist] = 0
+      artist_count[artist] += 1
+    }
+  artist_count
+  end
+end
