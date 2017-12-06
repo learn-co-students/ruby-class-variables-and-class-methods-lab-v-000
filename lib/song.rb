049c3d9a462a029e39965177908bb4236faa5299
Hoon Lee <yl.hoony@gmail.com>
2017-12-06 18:18:05 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+
+  attr_reader :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @@count += 1
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    result = Hash.new
+    @@genres.uniq.map { |genre| result[genre] = @@genres.count(genre) }
+    result
+  end
+
+  def self.artist_count
+    result = Hash.new
+    @@artists.uniq.map { |artist| result[artist] = @@artists.count(artist) }
+    result
+  end
+
+
+end
