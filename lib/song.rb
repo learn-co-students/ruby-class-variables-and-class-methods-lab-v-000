3b17602faae7af5ef7031e196d0276137ae86645
Eric Lin <esotericlin@gmail.com>
2017-12-11 22:42:34 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  @@genres = []
+  @@artists = []
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+end
