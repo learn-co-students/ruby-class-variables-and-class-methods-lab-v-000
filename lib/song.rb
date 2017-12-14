2f506b06884e166bc7cf1095667aec918463bb7d
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-14 05:34:17 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry'
+class Song
+  attr_accessor :name, :genre, :artist
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash.include?(artist)
+        artist_hash[artist] +=  1
+      else
+        artist_hash[artist] = 1
+      end
+    end
+      artist_hash
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash.include?(genre)
+        genre_hash[genre] +=  1
+      else
+        genre_hash[genre] =  1
+      end
+    end
+      genre_hash
+  end
+
+end
