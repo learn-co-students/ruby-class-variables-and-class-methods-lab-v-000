2171ff3a3cdbd43ad6a7ad1c7f3189a0f013f2af
tanelam <tania.e.aparicio@gmail.com>
2017-12-13 23:07:05 -0500

    done

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require 'pry'
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+ def initialize(name, artist, genre)
+   @name = name
+   @artist = artist
+   @genre = genre
+   @@count += 1
+   @@artists << self.artist
+   @@genres << self.genre
+ end
+
+ def self.count
+   @@count
+ end
+
+ def self.artists
+   @@artists.uniq
+ end
+
+ def self.genres
+   @@genres.uniq
+ end
+
+ def self.genre_count
+   @@genres.each_with_object(Hash.new(0)) { |genre, counts| counts[genre] += 1 }
+ end
+
+ def self.artist_count
+   @@artists.each_with_object(Hash.new(0)) { |artist, counts| counts[artist] += 1 }
+end
+
+end
