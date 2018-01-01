dcbc3cb9c274aa8d96db6851959edd9e5fea666d
funemployeddad <funemployeddad@gmail.com>
2018-01-01 16:33:51 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -15,18 +15,39 @@ class Song
   end
 
   def self.count
-    return @@count
+    @@count
   end
 
   def self.artists
-    return @@artists.uniq
+    @@artists.uniq
   end
 
   def self.genres
-    return @@genres.uniq
+    @@genres.uniq
   end
 
-  def genre_count
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
+
+  def self.artist_count
+    artist_count = {}
+      @@artists.each do |artist|
+        if artist_count[artist]
+          artist_count[artist] += 1
+        else
+          artist_count[artist] = 1
+        end
+      end
+      artist_count
+    end
 
 end
