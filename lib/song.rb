c53c195457a833d41d549a7c310c1bd44d029df5
funemployeddad <funemployeddad@gmail.com>
2018-01-01 16:00:49 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    return @@count
+  end
+
+  def self.artists
+    return @@artists
+  end
+
+
+
+end
