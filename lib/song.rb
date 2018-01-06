d4ef80435cd1f0c1dd6b14f60b7c68f508a16476
kim alban <kimalbans@yahoo.com>
2018-01-06 18:20:31 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+  @@count = 0
+
+  def self.count
+    @@count
+  end
+
+  @@genres = []
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ; total }
+  end
+
+  @@artists = []
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ; total }
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
+end
