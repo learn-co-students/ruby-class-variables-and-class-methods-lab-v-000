eab0cb51c849790279981b9f1e542c37f13ed825
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-10 18:58:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -62,3 +62,19 @@ class Song
 
 
 end
+=begin
+Avis sln:
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+
+=end
