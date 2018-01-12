0c740f7c3dc37ef81b157b2f372401c4b9dedfd2
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-12 22:40:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 
 def callsong(clas)
+###THIS METHOD IS FOR TESTING PURPOSE ONLY
   song  = clas.new("aa", "bb", "cc")
   song  = clas.new("aa", "bb", "cc")
   Song.count
   Song.artists
   Song.genres
   Song.genre_count
-
 end
 class Song
   attr_accessor  :name, :artist, :genre
@@ -25,6 +25,7 @@ class Song
     @@genres       << genre
     @@count         += 1
   end
+
   def self.count
     puts @@count
     @@count
@@ -33,35 +34,45 @@ class Song
   def self.genre_count
     @@genres.each do |genre|
       if @@genre_count[genre] == nil
-          puts "false"
-          @@genre_count[genre] = 1
-          puts @@genre_count
-
+        @@genre_count[genre] = 1
       else
-        @@genre_count.each do |k, v| if k == genre
-          puts k
-
-          puts v
-          @@genre_count[k] = v + 1
-          puts @@genre_count
-                end
-              end
+        @@genre_count.each do |cgenre, cgenre_value|
+          if cgenre == genre
+            @@genre_count[cgenre] = cgenre_value + 1
+          end
+        end
       end
     end
-    @@genre_count
-  end
+      @@genre_count
+    end
 
-  def self.artist_count
-    @@artist_count
-  end
+    def self.artist_count
+      @@artists.each do |artist|
+        if @@artist_count[artist] == nil
+          @@artist_count[artist] = 1
+        else
+          @@artist_count.each do |cartist, cartist_value|
+            if cartist == artist
+              @@artist_count[artist] = cartist_value + 1
+            end
+          end
+        end
+      end
+        @@artist_count
+      end
 
-  def self.artists
-    @@artists.uniq
-  end
+    def self.artist_count
+      @@artist_count
+    end
 
-  def self.genres
-    @@genres.uniq
+    def self.artists
+      @@artists.uniq
+    end
+
+    def self.genres
+      @@genres.uniq
+    end
+
   end
-end
 
 callsong(Song)
