5effe7321964943f675bfe17fe3c8277bb33774b
avelineamour <razelle@satyaorthopedic.com>
2018-01-16 21:38:18 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+
+
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+
+  def self.genre_count
+    genrehash = {}
+    @@genres.uniq.each {|g| genrehash[g] = @@genres.count{|i| i == g }}
+    genrehash
+  end
+
+  def self.artist_count
+    artisthash = {}
+    @@artists.uniq.each {|a| artisthash[a] = @@artists.count{|i| i == a }}
+    artisthash
+  end
+
+end
