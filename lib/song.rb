2465f3dd0cc58e41fac394c8c8677e2b236fbfb5
Ron Sala <ron.sala@gmail.com>
2018-01-25 03:37:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,16 @@ class Song
   end
 
   def self.genre_count
-    @@genres
-    @@genre_count
+    @@genres.inject(Hash.new(0)) do |hash, e|
+      hash[e] += 1
+      hash
+    end
+  end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) do |hash, e|
+      hash[e] += 1
+      hash
+    end
   end
 end
