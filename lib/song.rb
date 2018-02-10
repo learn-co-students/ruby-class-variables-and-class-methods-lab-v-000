b7efced4d0c149519fec5f31b487bdcf61ee3ef0
Jayon Kim <jpkim921@gmail.com>
2018-02-10 20:08:07 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+
+
+  def count
+    self.length
+  end
+
+
+
+end
