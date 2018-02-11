13ca7528ed274652baa09416f3c0d3de34a02f4c
Jayon Kim <jpkim921@gmail.com>
2018-02-11 00:15:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_reader :name, :artist, :genre
 
@@ -13,14 +15,37 @@ class Song
     @@count += 1
     @@artists << artist
     @@genres << genre
+
+
   end
 
-
-
-  def count
-    self.length
+  def self.count
+    @@count
   end
 
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres_count = Hash.new(0)
+    @@genres.each do |genre|
+      @@genres_count[genre] += 1
+    end
+    @@genres_count
+  end
+
+  def self.artist_count
+    @@artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      @@artist_count[artist] += 1
+    end
+    @@artist_count
+  end
 
 
 end
