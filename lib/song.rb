1e76ca34d63a4b90595957733ae6ce965c981609
Aderianna T Williams <aderiannaw@yahoo.com>
2018-02-13 20:13:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,15 @@ attr_accessor :name, :artist, :genre
     end
     genre_count
   end
-    def artist_count
+    def self.artist_count
+       artist_count = {}
+      @@artists.each do |artist|
+        if artist_count.include?(artist) then artist_count[artist] += 1 else artist_count[artist] = 1
+      end
+    end
+    artist_count
+        
+      
 
     end
 end
