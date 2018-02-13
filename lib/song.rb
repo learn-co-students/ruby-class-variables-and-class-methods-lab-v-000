278ae6857f3192ce48606545cf87b1f1a6c35973
A T Williams <aderiannaw@yahoo.com>
2018-02-13 15:17:36 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+      @@count
+    end
+
+    def self.artists
+        @@artists.uniq!
+      end
+
+    def self.genres
+      @@genres.uniq!
+    end
+
+    def genre_count
+      genre = {}
+      genre.collect do |genre|
+      if gerne.[genre] then genre[genre] =+ 1 else genre[genre] = 1
+      end
+    end
+  end
+    def artist_count
+
+    end
+end
