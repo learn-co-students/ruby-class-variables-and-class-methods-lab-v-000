e2367c88487470f6ee158ecf38fd8ddfae7fb81d
Harley Rayner <harleymteden@gmail.com>
2018-02-16 21:09:05 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << @artist
+    @genre = genre
+    @@genres << @genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count_hash = {}
+    @@genres.each do |genre|
+        if genre_count_hash.include?(genre)
+          genre_count_hash[genre] += 1
+        else
+          genre_count_hash[genre] = 1
+        end
+    end
+    genre_count_hash
+  end
+
+  def artist_count
+
+  end
+
+end
