aa2344ea8bd04b77c861cb751a11ecffcb2ca7cf
Natalie Halawani <hala1224@gmail.com>
2018-02-20 02:51:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,49 @@
 class Song
-  attr_accessor :song_name, :artist, :genre
-  @@song_count=0
-  @@genres_count=0
-  @@artist_count=0
-  @@genres=[]
-  @@artists=[]
-  def initialize(song_name,artist,genre)
-    @@song_count+=1
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genres_count = 0
+  @@artists_count = 0
+
+  def initialize(name, artist, genre)
+    @@count+=1
     @@genres_count+=1
-    @@artist_count+=1
-    @song_name=song_name
-    @artist=artist
-    @genre=genre
-    @@genre << @genre
-    @@artist << @artist
+    @@artists_count+=1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
   end
   def self.count
-    @@song_count
+     @@count
   end
-  def genre_count
-    @@genre.uniq!.length
-  end
-  def artist_count
-    @@artist.uniq!.length
-  end
+  def self.genres
+    @@genres.uniq!
+   end
+   def self.artists
+    @@artists.uniq!
+        # @@artists.uniq!.length
+   end
+   def self.genre_count
+      my_hash={}
+      i=0
+      my_array=@@genres
+      while i < my_array.length
+           my_hash.store(my_array[i],my_array.count(my_array[i]))
+           i+=1
+       end
+       my_hash
+   end
+   def self.artist_count
+     my_hash={}
+   i=0
+   my_array=@@artists
+    while i < my_array.length
+      my_hash.store(my_array[i],my_array.count(my_array[i]))
+      i+=1
+    end
+    my_hash 
+   end
+end
