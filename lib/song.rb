40c6e7ea868088aef75bf3e02137d04456a64147
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-28 00:00:46 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres = @@genres.uniq
+    @@genres
+  end
+
+  def self.artists
+    @@artists = @@artists.uniq
+    @@artists
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash.include?(genre)
+        genre_hash[genre] += 1
+      else
+        genre_hash[genre] = 1
+      end
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash.include?(artist)
+        artist_hash[artist] += 1
+      else
+        artist_hash[artist] = 1
+      end
+    end
+    artist_hash
+  end
+
+end
