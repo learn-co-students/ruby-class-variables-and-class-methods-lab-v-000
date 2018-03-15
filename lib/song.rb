85442e452f26bd517fbfa57d6d9c650f21f21dde
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-15 22:30:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,51 @@
 class Song
   attr_accessor :name, :artist, :genre
-   # @@count = 0
-   # @@genres = []
-   # @@artists = []
+   @@count = 0
+   @@genres = []
+   @@artists = []
+   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    # @@genres = genres
+    @@genres << genre
+    @@artists << artist
   end
   
-  # def self.count
-  #   @@count
-  # end
+  def self.count
+  @@count
+  end
   
-  # def self.genres
-  #   @@genres unique
-  # end
+  def self.genres
+  @@genres.uniq
+  end
   
-  # def self.artists
-  #   @@artists unique
-  # end
+  def self.artists
+  @@artists.uniq
+  end
   
-  # def self.genre_count
-  #   hash of key value pairs of genres and their count using @@genres
-  # end
+   def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
-  # def artist_count
-  #   hash of key value pairs of artists and their count using @@arists
-  # end
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
