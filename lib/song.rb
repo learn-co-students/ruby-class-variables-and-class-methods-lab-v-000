cb4b3bf23867ec6a36236ad1b68a6c172b2ead01
Madeline Stark <madeline.stark1@gmail.com>
2018-04-10 19:44:33 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    Hash[*@@genres.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+  end
+
+  def self.artist_count
+    Hash[*@@artists.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+  end
+
+end
