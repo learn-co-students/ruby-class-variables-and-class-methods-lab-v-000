901f8678d27a7f72c16f7a486adabb990f9b2fb3
Andrey Syroyezhkin <asyroyez@gmail.com>
2018-04-14 13:40:49 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,23 +30,15 @@ class Song
 
   def self.genre_count
     genre_count = Hash.new
-    @@genres.map do |genre|
-      if !genre_count[genre]
-        genre_count[genre] = 0
-      end
-      genre_count[genre] += 1
-    end
+    @@genres.map { |genre| !genre_count[genre] ?
+                            genre_count[genre] = 1 : genre_count[genre] += 1 }
     genre_count
   end
 
   def self.artist_count
     artist_count = Hash.new
-    @@artists.map do |artist|
-      if !artist_count[artist]
-        artist_count[artist] = 0
-      end
-      artist_count[artist] += 1
-    end
+    @@artists.map { |artist| !artist_count[artist] ?
+                     artist_count[artist] = 1 : artist_count[artist] += 1 }
     artist_count
   end
 end
