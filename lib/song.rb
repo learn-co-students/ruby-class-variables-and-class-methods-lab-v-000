936104c26841089006002f3b427b8206ab992f55
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-24 18:02:11 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.uniq.each do |genre|
+      genre_count[genre] = @@genres.count(genre)
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.uniq.each do |artist|
+      artist_count[artist] = @@artists.count(artist)
+    end
+    artist_count
+  end
+
+end
