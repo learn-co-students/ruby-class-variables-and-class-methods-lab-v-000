8bc3a0abb0bf6aa6a40f857e2ecab0bbdb33fa2a
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-26 23:47:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,44 @@ class Song
   attr_accessor :name, :artist, :genre
 
 @@count = 0
+@@artists = []
+@@genres = []
 
-def initialize
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
   @@count += 1
+  @@artists << artist
+  @@genres << genre
 end
 
 def self.count
-  @@count +=1
+  @@count
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.genre_count
+  genre_list = {}
+  @@genres.each do |genre_names|
+    genre_list[genre_names] = 0
+    counter += 1
+  end
+end
+
+def self.artist_count
+  artist_list = {}
+  @@artists.each do |artist_names|
+    artist_list[artist_names] = 0
+    counter += 1
+  end
 end
 
 end
