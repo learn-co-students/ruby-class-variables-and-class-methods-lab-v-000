60d4b4846233e40dcd4d5c9236804acf1c2353e7
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-27 03:11:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist, :genre
 
@@ -28,18 +29,28 @@ end
 
 def self.genre_count
   genre_list = {}
-  @@genres.each do |genre_names|
-    genre_list[genre_names] = 0
-    counter += 1
+  @@genres.each do |genre|
+    #binding.pry
+    if genre_list[genre]
+      genre_list[genre] +=1
+    else
+      genre_list[genre] =1
   end
+end
+  genre_list
 end
 
 def self.artist_count
   artist_list = {}
-  @@artists.each do |artist_names|
-    artist_list[artist_names] = 0
-    counter += 1
+  @@artists.each do |artist|
+    if artist_list[artist]
+      artist_list[artist] +=1
+    else
+      artist_list[artist] =1
   end
+end
+binding.pry
+  artist_list
 end
 
 end
