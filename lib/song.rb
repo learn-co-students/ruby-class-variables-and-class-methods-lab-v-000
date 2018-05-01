522bab314a08b9b70264cd72b29d1b6945e8f47b
Brent Bauman <brentbauman86@gmail.com>
2018-05-01 17:32:09 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+
+end
+
+def self.count
+@@count
+end
+
+def self.genre_count
+  @@genres.length
+end
+
+end
