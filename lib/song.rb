02633e3cb98d98c27c3c89c9ec933d287a67c565
Reno Sorensen <renojames19@gmail.com>
2018-05-05 01:41:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -42,4 +42,16 @@ class Song
     genre_count
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do | artist |
+      if !artist_count.include?(artist)
+        artist_count[artist] = 1
+      else
+        artist_count[artist] += 1
+      end
+    end
+    artist_count
+  end
+
 end
