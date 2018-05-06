f2b0391072d05d0b2c372cb6f8ba99457e82e578
Richard Cho <richardcho100@gmail.com>
2018-05-06 03:25:41 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+require 'pry'
+class Song
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@count += 1
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+#["rap", "rap", "pop"]
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash[genre] == nil
+        genre_hash[genre] = 1
+      else
+        genre_hash[genre] += 1
+      end
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash[artist] == nil
+        artist_hash[artist] = 1
+      else
+        artist_hash[artist] += 1
+      end
+    end
+    artist_hash
+  end
+
+end
