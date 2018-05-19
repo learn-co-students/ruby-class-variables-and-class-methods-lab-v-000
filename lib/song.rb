18f9698b1dbf399cf3e04edbb03277eaba5b1908
Dale Terrell <tragicid@gmail.com>
2018-05-19 15:57:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,22 @@
-def Song 
-  Song.count
-  Song.artists
-  Song.genres 
+class Song
+
+  def Song.count=(count)
+    @Song.count=count
+  end
+  
+  def Song.artists=(artists)
+    @Song.artists=artists
+  end
+  
+  def Song.genres=(genres) 
+    @Song.genres=genres
+  end
+  
+  def Song.genre_count(genre_count)
+    @Song.genre_count=genre_count
+  end
+  
+  def Song.artist_count(artist_count)
+    @Song.artist_count=artist_count
+  end
 end
