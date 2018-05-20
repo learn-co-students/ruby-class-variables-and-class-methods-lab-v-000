f6d25d40daedb812a1edebf8e1eb1dce874378ff
Dale Terrell <tragicid@gmail.com>
2018-05-20 04:03:08 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,84 +1,53 @@
-require "pry"
 class Song
-  @@count=0
-  @@artists=[]
-  @@genres=[]
-  
-  def initialize(name,artist,genre)
-    @name=name
-    @artist=artist
-    @@artists << artist
-    
-    @genre=genre
+  attr_accessor :name, :artist, :genre
+  attr_writer :count
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
     @@genres << genre
-    
-    @@count+=1
+    @@artists << artist
   end
-    
+
   def self.count
     @@count
   end
-  
-  def name=(name)
-    @name=name
-  end
-  
-  def name 
-    @name
-  end
-  
-  def artist=(artist)
-    @artist=artist
-  end
-  
-  def artist
-    @artist
-  end
-  
-  def self.artists
-    artists.uniq{|artist|}
-``end
-  
-  def genre=(genre) 
-    @genre=genre
-  end
-  
-  def genre
-    @genre
-  end
-  
-  def genre_count=(genre_count)
-    genre_count=genre_count
-  end
-  
-  def genre_count
-    @genre_count
-  end
-  
-  def artist_count=(artist_count)
-    @artist_count=artist_count
-  end
-  
-  def artist_count
-    @artist_count
-  end
-  
-  attr_accessor :name, :artist, :genre
 
-  
-  def genre_count=(genre_count)
-    @genre_count=genre_count
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
   
-  def genre_count
-    @genre_count
-  end
-  
-  def artist_count=(artist_count)
-    @artist_count=artist_count
-  end
-  
-  def artist_count
-    @artist_count
+  def self.artist_count
+  artist_count = {}
+  @@artist.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
