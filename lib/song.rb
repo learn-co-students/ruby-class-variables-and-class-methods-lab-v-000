bce2449a5e8ca24b83a1bc1a0b55d26f76c5e256
Christia T <christiatidwell@bellsouth.net>
2018-05-22 13:37:22 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists<<artist
+    @@genres<<genre
+  end
+
+
+
+  def self.count
+  @@count
+end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+
+    genre_count = Hash.new(0)
+    @@genres.each{|genre| genre_count[genre] +=1}
+    genre_count
+
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each{|artist| artist_count[artist] +=1}
+    artist_count
+  end
+
+end
