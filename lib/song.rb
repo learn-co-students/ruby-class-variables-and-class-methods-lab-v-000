58db213290733d1034f9d6979c040f28c1e93830
Aziz Kosber <a.kosber@gmail.com>
2018-06-05 00:55:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,16 @@ class Song
     end
     genre_hash
   end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash.include?(artist)
+        artist_hash[artist] += 1
+      else
+        artist_hash[artist] = 1
+      end
+    end
+    artist_hash
+  end
 end
