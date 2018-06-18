e1928b69c6b18e4ed4d513b2dfafa150ef4576be
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-18 17:47:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -39,27 +39,24 @@ class Song
     genre_count = {}
     @@genres.each do | genre |
       if genre_count[genre]
-        genre_count += 1
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
     end
+    genre_count
   end
   
   def self.artist_count
     artist_count = {}
     @@artists.each do | artist |
       if artist_count[artist]
-        artist_count += 1
+        artist_count[artist] += 1
       else
-        artist_count = 1
+        artist_count[artist] = 1
       end
     end
-    
-
-  
- 
- 
+    artist_count
 end
   
 end
