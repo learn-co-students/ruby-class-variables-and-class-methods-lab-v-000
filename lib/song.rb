1c9cbf8237968b6063ceba9fbfffd74a9fce1270
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-18 16:16:37 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  
+  @@count = 0
+  
+  @@genres = []
+  
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do | genre |
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+  end
+  
+  @@artists = []
+  
+  def self.artist_count
+    @@artists.each do | artist |
+      if artist_count[artist]
+        artist_count += 1
+      else
+        artist_count = 1
+      end
+    end
+    
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.count
+    @@count
+  end
+ 
+end
+  
+  attr_accessor :name, :artist, :genre
+end
