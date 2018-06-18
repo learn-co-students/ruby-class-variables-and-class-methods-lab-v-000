48511a6bb7dc286198ddae2d3ed211560b8f7c4a
Valencia Caldwell <vkcaldwell@gmail.com>
2018-06-18 20:15:15 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song 
+  @@count = 0
+  
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+   attr_accessor :name, :artist, :genre
+
+  
+  def self.count 
+    @@count
+  end 
+  
+  @@genres = []
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  @@artists = []
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+end
+  
+
