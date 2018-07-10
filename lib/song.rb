a68ff9149caa0197df5bbec479598873b975ebb9
Aziz Kosber <a.kosber@gmail.com>
2018-07-10 04:18:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,52 +1,47 @@
 require 'pry'
 class Song
-  attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
   @@genres = []
 
-  def initialize(name, artist, genre)
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
-    @name = name
-    @artist = artist
-    @genre = genre
-  end
-
   def self.count
     @@count
-  end
-
-  def self.artists
-    @@artists.uniq
   end
 
   def self.genres
     @@genres.uniq
   end
 
+  def self.artists
+    @@artists.uniq
+  end
+
   def self.genre_count
-    genre_hash = {}
+    @@genre_incidence = {}
     @@genres.each do |genre|
-      if genre_hash.include?(genre)
-        genre_hash[genre] += 1
-      else
-        genre_hash[genre] = 1
-      end
+      @@genre_incidence[genre] ||= 0
+      @@genre_incidence[genre] += 1
     end
-    genre_hash
+    @@genre_incidence
   end
 
   def self.artist_count
-    artist_hash = {}
+    @@artist_incidence = {}
     @@artists.each do |artist|
-      if artist_hash.include?(artist)
-        artist_hash[artist] += 1
-      else
-        artist_hash[artist] = 1
-      end
+      @@artist_incidence[artist] ||= 0
+      @@artist_incidence[artist] += 1
     end
-    artist_hash
+    @@artist_incidence
+  end
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << self.genre
+    @@artists << self.artist
   end
 end
