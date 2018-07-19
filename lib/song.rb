3f19c1a5b12f51b2c230a7b16b0d335b5652e893
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-19 11:29:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+
+
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  
+    @@count += 1
+    @@genres << genre  
+    @@artists << artist
+   end
+   
+   
+   
+   def self.count
+     @@count
+   end
+   
+   def self.genres
+     @@genres.uniq
+   end
+   
+   def self.artists
+     @@artists.uniq
+   end
+    
+    def self.genre_count
+      genre_count = {}
+      
+      @@genres.each do |genre| 
+       if genre_count[genre] 
+        genre_count[genre] += 1
+       else
+        genre_count[genre] = 1
+      end 
+    end
+    genre_count
+    end
+    
+    def self.artist_count
+      artist_count = {} 
+      
+      @@artists.each do |artists| 
+        if artist_count[artists]
+          artist_count[artists] += 1
+        else
+          artist_count[artists] = 1
+    end
+  end
+  artist_count
+end
+end
