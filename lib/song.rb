f8643e9eeec3d750b869808e7448f31737331462
Alex Roman <elpitbull6989@gmail.com>
2018-07-19 04:44:14 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,44 +1,48 @@
 require 'pry'
 class Song
-
-  @@count = 0
+  @@artists = []
   @@genres = []
-  @@artists= []
-  attr_accessor :name, :artist, :genre
+  @@count  = 0
+
+  attr_accessor :name,:artist,:genre
 
   def initialize(name,artist,genre)
+    @@count +=1
     @name = name
     @artist = artist
     @genre = genre
     @@artists << artist
     @@genres << genre
-    @@count += 1
+
+
   end
 
+  def self.count
+    @@count
+  end
 
   def self.artists
     @@artists.uniq!
-
 
   end
 
   def self.genres
     @@genres.uniq!
-  end
-  def self.count
-    @@count
+
   end
 
   def self.genre_count
     genre_count = {}
-    @@genres.each { |g| genre_count[g] = @@genres.count(g)}
+    @@genres.each { |genre| genre_count[genre] = @@genres.count(genre)}
     genre_count
   end
 
   def self.artist_count
     artist_count = {}
     @@artists.each {|artist| artist_count[artist] = @@artists.count(artist)}
+
     artist_count
   end
+
 
 end
