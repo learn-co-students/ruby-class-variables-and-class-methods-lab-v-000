18b490e22f398b1a57d1d77174431e8e60a82432
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-07 02:20:15 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initalize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+    return @@genres.uniq
+  end
+
+  def artists
+    return @@artists.uniq
+  end
+
+  def genre_count
+    @@genres.each do |genre|
+      count = {}
+      if count.has_key?(genre)
+        count[genre] += 1
+      else
+        count[genre] = 1
+      end
+      return count
+    end
+  end
+end
