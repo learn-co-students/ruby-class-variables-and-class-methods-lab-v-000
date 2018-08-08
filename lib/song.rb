2e121109df03799fad0b4bfcc0f66c8942001191
Claire Harmer <claire.f.harmer@gmail.com>
2018-08-08 23:18:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
@@ -21,31 +22,23 @@ class Song
     @@artists.uniq
   end
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-    if genre_count[genre]
-    genre_count[genre] +=1
-    else
-    genre_count[genre] = 1
-    end
+  def self.genres
+    @@genres.uniq
   end
-    genre_count
+
+  def self.genre_count
+    @genre_count = Hash.new(0)
+    @@genres.each do |genre|
+    @genre_count[genre] += 1
+  end
+    @genre_count
   end
 
   def self.artist_count
-    artist_count = {}
+    @artist_count = Hash.new(0)
     @@artists.each do |artist|
-    if artist_count[artist]
-    artist_count[artist] += 1
-    else
-    artist_count[artist] = 1
-    end
+    @artist_count[artist] += 1
   end
-    artist_count
-  end
-
-  def self.genres
-   @@genres.uniq
+    @artist_count
   end
 end
