0408d83834199219b374affafde5814a16429e23
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-08-09 01:09:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,45 @@
+require 'pry'
+
 class Song 
+    
   attr_accessor :name, :artist, :genre 
   
   @@count = 0
   @@genres = []
+  @@artists = []
+  @@genre_count = {}
   
-  def intialize(name, artist, genre)
-    @count + 1 
+  def initialize(name, artist, genre)
     @name = name
-    @artist = artist
     @genre = genre
+    @artist = artist
+    
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.genres 
+    @@genres.uniq
   end
   
   def self.count
     @@count
   end
   
-  def self.genres(genre)
-    @@genres.include?(genre)
-      return nil 
-    else 
-      @@genres << genre 
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    @@genres.each do |genre, count|
+      if genre.uniq
+       @@genre_count[genre] =  @@genres.count
+       end
+       @@genre_count
     end
   end
-      
-    
+end
+
   
   
