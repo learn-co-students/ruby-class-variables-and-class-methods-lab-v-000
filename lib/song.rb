452b8a778d04414b54605e18c765a8ecb55cac87
Achsah Abraham <achsahmp@gmail.com>
2018-08-21 18:15:56 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << genre
+    @@artist = artist
+    @@artists << artist
+    @name = name
+    @genre = genre
+    @artist = artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each do |genre|
+      @@genre_count[genre] = @@genres.count(genre)
+    end
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      @@artist_count[artist] = @@artists.count(artist)
+    end
+    @@artist_count
+  end
+end
