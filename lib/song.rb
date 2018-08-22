eaaae6a35e1981e2aae511b7b8a28a8d4c13858e
Daniel Schoch <d.schoch90@gmail.com>
2018-08-22 19:30:13 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      artist_count.store(artist, artist_count[artist]+1)
+    end
+    artist_count
+  end
+
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each do |genre|
+      genre_count.store(genre, genre_count[genre]+1)
+    end
+    genre_count
+  end
+end
