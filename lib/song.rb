8e139e6d33aabd23422ec9ec81adc65593d05c1e
Laura Darlak <laura@polyporedesign.com>
2018-08-25 00:06:12 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    freqs = Hash.new(0)
+    @@genres.each {|genre| freqs[genre] += 1}
+    freqs
+  end
+
+  def self.artist_count
+    artist_count_h = Hash.new(0)
+    @@artists.each {|artist| artist_count_h[artist] += 1}
+    artist_count_h
+  end
+
+end
