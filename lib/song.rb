e7d959d52b6e3c78af70bc1e49178b2d59bed540
Susan Meyers <smmeyers24@gmail.com>
2018-09-08 14:16:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,6 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@artists_count = 0
-  @@genres_count = 0
-
 
   attr_accessor :name, :artist, :genre
 
@@ -16,10 +13,6 @@ class Song
     @@count += 1
     @@artists << artist
     @@genres << genre
-    @@artists_count += 1
-    @@genres_count += 1
-
-
   end
 
   def self.count
@@ -37,21 +30,27 @@ class Song
 
 
   def self.artist_count
-    @@artist.each do |genres|
-      array[genre] = artist.genre.count
+    hash = {}
+    @@artists.each do |artist|
+      if !hash[artist]
+        hash[artist] = 1
+      else
+        hash[artist] += 1
+      end
     end
+    hash
   end
-
-
 
   def self.genre_count
     hash = {}
     @@genres.each do |genre|
-
-      hash[genre] = genre.song.count
-
+      if !hash[genre]
+        hash[genre] = 1
+      else
+        hash[genre] += 1
+      end
     end
-
+    hash
   end
 end
 
