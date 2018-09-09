afa837cd596e5bb5a742ce295f374a8e257b6e19
Ashley Wong <ashwong04@gmail.com>
2018-09-09 23:45:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,15 @@
 class Song
   
+  @@song_count = 0
+  
   def initialize (name, artist, genre)
     @name= name
     @artist= artist
     @genre= genre
+  end
+  
+  def self.count
+    @@song_count
   end
   
 end
