07e97a3eee3ec1c4682bc4b4322b56c664c1e466
Ashley Wong <ashwong04@gmail.com>
2018-09-09 23:55:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,29 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  @@song_count = 0
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize (name, artist, genre)
     @name= name
     @artist= artist
     @genre= genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
   end
   
   def self.count
-    @@song_count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
   end
   
 end
