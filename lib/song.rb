87f240c1754121caab700dd42b80c8a5dea68767
J Fern <jfern20@gmail.com>
2018-09-19 02:28:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,18 @@
 class Song 
-  attr_accessor :name, :artist, :genre 
+  attr_accessor :name, :artist, :genre
   
   @@count = 0 
-  @@artist = [] 
-  @@genre = [] 
+  @@artists = []
+  @@genres = [] 
   def initialize(name, artist, genre )
      @name = name
+     
      @artist = artist
-     @genre = genre 
+     @@artists << artist  
+     
+     @genre = genre
+     @@genres << genre 
+     
      @@count += 1 
   end 
   
@@ -15,18 +20,30 @@ class Song
     @@count
   end 
   
-  def self.genre
-  @@genre
+  def self.genres
+  @@genres.uniq 
   end 
   
-  def self.artist
-  @@artist
+  def self.artists 
+  @@artists.uniq
   
   end 
   
   def self.genre_count
-   
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count [genre]
+        genre_count[genre] +=1 
+      else 
+        genre_count[genre] = 1 
+      end 
+    end 
+    genre_count
   end 
+   
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
   
 end 
 
