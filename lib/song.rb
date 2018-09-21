53970d99adfa3cff3efa19dce511141781ed6685
Dana Palmer <roiai4@hotmail.com>
2018-09-21 04:30:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
+  #these are our class vars which keep track of the entire class' worth of count, artists, and genres
 
   def initialize(name, artist, genre)
     @name = name
@@ -14,14 +15,18 @@ class Song
     @@genres << genre
     @@artists << artist
   end
+  #We are initializing with name, artist, and genre, but also incrementing our count of songs by one for each instance.  Also we are adding the genre and artist in the initialize method to the class methods.
 
 def self.count
   @@count
 end
+#when typing Song.count it will give us the current count of instances, or how many songs we have created so far.
 
 def self.artists
   @@artists.uniq
 end
+#This method shows us the unique artist class method
+
 
 def self.genres
   @@genres.uniq
@@ -30,13 +35,18 @@ end
 def self.artist_count
   artist_count = {}
   @@artists.each do |artist|
+    #iterates over the @@artists array
   if artist_count[artist]
+    #if the artist exists
     artist_count[artist] += 1 
+    #increment by one
   else
     artist_count[artist] = 1 
+    #if the artist already exists, don't increment
   end
 end
 artist_count
+#returns artist_count
 end
 
 def self.genre_count
