80ea48341cfcda519dd97ac6972cfc2aa2db959a
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-21 20:43:25 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count=0
+  @@genres=[]
+  @@artists=[]
+  
+  def initialize(name, artist, genre)
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def count
+    @@count
+  end
+  
+  def artists
+    self.artists
+  end
+  
+  def genres
+    self.genres
+  end
+  
+  def genre_count
+    self.genre each do |key, value|
+      self[key].count
+      
+    end
+  end
+  
+  def artist_count
+    
+  end
+  
+  
+  
+  
+  
+end
