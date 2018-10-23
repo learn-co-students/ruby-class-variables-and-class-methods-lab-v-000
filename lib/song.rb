da9aef209ffe2ffacfe916744118d7cab1193e1d
Sophie Gu <sophieqgu@gmail.com>
2018-10-23 15:40:56 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song 
+  attr_reader :name, :artist, :genre
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@count += 1 
+    @@artists << artist 
+    @@genres << genre
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.artists 
+    @@artists.uniq  
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.genre_count 
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] = @@genres.count(genre)
+    end 
+    genre_count
+  end 
+  
+  def self.artist_count 
+    artist_count = {}
+    @@artists.each do |artist|
+      artist_count[artist] = @@artists.count(artist)
+    end 
+    artist_count
+  end 
+end 
