45a6e73c35e08aea631dc37c145ee105c4ab4be9
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-24 17:30:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,28 +6,28 @@ class Song
   
   def initialize(name, artist, genre)
     @@count +=1
-             
     @@genres << genre
- 
     @@artists << artist
+    @name=name
+    @artist=artist
+    @genre=genre
   end
   
-  def count
+  def self.count
     @@count
   end
   
-  def artists
-    self.artists
+  def self.artists
+    @@artists.uniq
   end
   
-  def genres
-    self.genres
+  def self.genres
+    @@genres.uniq
   end
   
   def genre_count
     self.genre each do |key, value|
       self[key].count
-      
     end
   end
   
@@ -35,6 +35,9 @@ class Song
     
   end
   
+  def name
+    @name
+  end
   
   
   
