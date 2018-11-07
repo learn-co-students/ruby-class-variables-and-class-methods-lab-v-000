62651caa601ff0331de2c39bdc1093b1d1230ccc
Lois Lee <lois.lee@me.com>
2018-11-07 00:25:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,21 @@
+require 'pry'
 class Song
+
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
   @@genres = []
   @@artists = []
-  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre
     @@artists << artist
+    @@genres << genre
   end
+
 
   def self.count
     @@count
@@ -26,22 +29,12 @@ class Song
     @@artists.uniq
   end
 
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist] #what does this do?
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
   def self.genre_count
     genre_count = {}
+
     @@genres.each do |genre|
-      if genre_count[genre] #what does this do?
+      binding.pry
+      if genre_count[genre]
         genre_count[genre] += 1
       else
         genre_count[genre] = 1
@@ -50,4 +43,16 @@ class Song
     genre_count
   end
 
+  def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
