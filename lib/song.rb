b7f83b8f947050d129d1ceddcd66fdf91ac9094d
Lois Lee <lois.lee@me.com>
2018-11-07 00:26:16 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -31,9 +31,7 @@ class Song
 
   def self.genre_count
     genre_count = {}
-
     @@genres.each do |genre|
-      binding.pry
       if genre_count[genre]
         genre_count[genre] += 1
       else
@@ -45,7 +43,6 @@ class Song
 
   def self.artist_count
     artist_count = {}
-
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
