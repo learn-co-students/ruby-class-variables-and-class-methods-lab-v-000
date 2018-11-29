877842bbc282f76aa260ef636cb6f08aa5954a5b
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-29 19:25:03 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    count = {}
+    @@genres.each do |genre|
+      if count.has_key?(genre)
+        count[genre] += 1
+      else
+        count[genre] = 1
+      end
+    end
+    count
+  end
+
+  def self.artist_count
+    count = {}
+    @@artists.each do |artist|
+      if count.has_key?(artist)
+        count[artist] += 1
+      else
+        count[artist] = 1
+      end
+    end
+    count
+  end
+end
