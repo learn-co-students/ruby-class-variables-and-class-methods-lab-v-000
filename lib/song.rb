cfbc8dea4b032b578adadd480082dee0f02ed311
David Anderson <matandersonlv@hotmail.com>
2019-01-10 23:23:52 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+
+  @@all = []
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@all << self
+    @@artists << artist
+    @@genres << genre
+
+    if @@genre_count[genre]
+      @@genre_count[genre] += 1
+    else
+      @@genre_count[genre] = 1
+    end
+
+    if @@artist_count[artist]
+      @@artist_count[artist] += 1
+    else
+      @@artist_count[artist] = 1
+    end
+
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+
+end
