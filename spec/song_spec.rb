6650520e2bc0543426b50804a6fc467df8b4b705
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 19:46:01 +0000

    Done.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-puts describe "Song" do
+ describe "Song" do
 
   let!(:lucifer) { Song.new("Lucifer", "Jay-Z", "rap" ) }
   let!(:ninety_nine_problems) { Song.new("99 Problems", "Jay-Z", "rap") }
@@ -14,36 +14,36 @@ puts describe "Song" do
     Song.class_variable_set(:@@genre_count, {})
 
   end
-  puts describe "#new" do
+ describe "#new" do
     it "takes in three arguments: a name, artist and genre" do
       expect{Song.new("hit me baby one more time", "Brittany Spears", "pop")}.to_not raise_error
     end
   end
 
-  puts describe "#name" do
+   describe "#name" do
     it "has a name" do
       expect(hit_me.name).to eq("hit me baby one more time")
     end
   end
 
-  puts describe "#artist" do
+  describe "#artist" do
     it "has an artist" do
       expect(hit_me.artist).to eq("Brittany Spears")
     end
   end
 
-  puts describe "#genre" do
+   describe "#genre" do
     it "has a genre" do
       expect(hit_me.genre).to eq("pop")
     end
   end
 
-  puts describe "class variables" do
+   describe "class variables" do
     it "has a class variable, @@count" do
       expect(Song.class_variable_get(:@@count)).to eq(3)
     end
 
-    puts it "has a class variable, @@artists, that contains all of the artists of existing songs" do
+     it "has a class variable, @@artists, that contains all of the artists of existing songs" do
       expect(Song.class_variable_get(:@@artists)).to match(["Jay-Z", "Jay-Z", "Brittany Spears"])
     end
 
@@ -52,31 +52,31 @@ puts describe "Song" do
     end
   end
 
-  puts describe ".count" do
+   describe ".count" do
     it "is a class method that returns that number of songs created" do
       expect(Song.count).to eq(3)
     end
   end
 
-puts  describe ".artists" do
+  describe ".artists" do
     it "is a class method that returns a unique array of artists of existing songs" do
       expect(Song.artists).to match(["Jay-Z", "Brittany Spears"])
     end
   end
 
-puts  describe ".genres" do
+ describe ".genres" do
     it "is a class method that returns a unique array of genres of existing songs" do
       expect(Song.genres).to match(["rap", "pop"])
     end
   end
 
-  puts describe ".genre_count" do
+   describe ".genre_count" do
     it "is a class method that returns a hash of genres and the number of songs that have those genres" do
       expect(Song.genre_count).to eq({"pop" => 1, "rap" => 2})
     end
   end
 
-puts   describe ".artist_count" do
+  describe ".artist_count" do
     it "is a class method that returns a hash of artists and the number of songs that have those artists" do
       expect(Song.artist_count).to eq({"Brittany Spears" => 1, "Jay-Z" => 2})
     end
