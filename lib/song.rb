e783b180ded52522e7256c32e7f9a835a39e5cd8
Drake Rentrop <drakerentrop@gmail.com>
2019-01-23 15:56:55 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+ def self.genre_count
+   genre_hash = Hash.new(0)
+   @@genres.each {|genre| genre_hash[genre] += 1}
+   genre_hash
+ end
+ def self.artist_count
+   artists_hash = Hash.new(0)
+   @@artists.each {|artist| artists_hash[artist] += 1}
+   artists_hash
+ end
+end
