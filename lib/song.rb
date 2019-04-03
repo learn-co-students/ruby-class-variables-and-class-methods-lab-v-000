91982a7be4ce211c14315b68db1494ab2336d6bc
Luis Herrera <herrera.lrh@gmail.com>
2019-04-03 18:12:52 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -21,37 +21,35 @@ class Song
     @@count
   end
 
-  def self.genres
-    @@genres.uniq
-  end
-
   def self.artists
     @@artists.uniq
   end
 
+  def self.genres
+    @@genres.uniq
+  end
+
   def self.genre_count
     genre_count = {}
-    @@genres.each {|g| if genre_count[g]
-      genre_count[g] += 1
-    else
-      genre_count[g] = 1
+    @@genres.each do |g|
+      if genre_count[g]
+        genre_count[g] +=1
+      else
+        genre_count[g] = 1
+      end
     end
-  }
-  genre_count
+    genre_count
   end
 
   def self.artist_count
     artist_count = {}
-    @@artists.each {|a|
+    @@artists.each do |a|
       if artist_count[a]
-      artist_count[a] += 1
-    else
-      artist_count[a] = 1
+        artist_count[a] +=1
+      else
+        artist_count[a] = 1
+      end
     end
-      }
     artist_count
   end
-
-
-
 end
