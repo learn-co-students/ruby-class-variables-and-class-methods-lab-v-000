d1c8813e585783c5a553f5ec777876061bf4ad9d
Zain <zain@halai.us>
2019-08-06 12:44:29 -0500

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
+	attr_accessor :name, :artist, :genre
+
 	@@count = 0
 	@@artists = []
 	@@genres = []
-
-	attr_accessor :name, :artist, :genre
 
 	def initialize(name, artist, genre)
 		@name, @artist, @genre = name, artist, genre
