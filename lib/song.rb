534e37e832c1da21914e410347bfa66b9fb97194
Zain <zain@halai.us>
2019-08-06 12:41:41 -0500

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
 	@@count = 0
 	@@artists = []
 	@@genres = []
+
 	attr_accessor :name, :artist, :genre
 
 	def initialize(name, artist, genre)
@@ -19,30 +20,16 @@ class Song
 		@@artists.uniq
 	end
 
-	def self.artist_count
-		artist_count = {}
-
-		@@artists.each do |artist|
-			artist_count[artist] ||= artist_count[artist] = 0
-			artist_count[artist] += 1
-		end
-
-		artist_count
-	end
-
 	def self.genres
 		@@genres.uniq
 	end
 
 	def self.genre_count
-		genre_count = {}
+		@@genres.each_with_object(Hash.new(0)) { |genre, memo| memo[genre] += 1 }
+	end
 
-		@@genres.each do |genre|
-			genre_count[genre] ||= genre_count[genre] = 0
-			genre_count[genre] += 1
-		end
-
-		genre_count
+	def self.artist_count
+		@@artists.each_with_object(Hash.new(0)) { |artist, memo| memo[artist] += 1 }
 	end
 
 end
