6faff08ecefa4172a2d97671e0d46308893d4be2
Julie  McManus <julieannesays@gmail.com>
2020-02-01 17:40:51 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
 
   @@count = 0
   @@artists = []
@@ -9,33 +9,21 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@count +=1
-    @@genres << genre
-    @@artists << artist
-  end
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+    end
 
   def self.count
     @@count
   end
 
   def self.artists
-    @@artists.uniq
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
+    @@artists.uniq!
   end
 
   def self.genres
-    @@genres.uniq
+    @@genres.uniq!
   end
 
   def self.genre_count
@@ -48,5 +36,17 @@ class Song
       end
     end
     genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
